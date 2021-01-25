81fa107b8c42528b318ff172191d46c35bf6bc90
MSI.windows <r.vadim.90@gmail.com>
2021-01-25 17:13:16 +0300

    added a folder with themes move CloseApplicationCommand to a separate file

--- LElWPF.Core/ViewModels/MainWindowViewModel.cs
+++ LElWPF.Core/ViewModels/MainWindowViewModel.cs
@@ -5,47 +5,25 @@ using System.Windows.Input;
 
 namespace LElWPF.Core.ViewModels
 {
-    class MainWindowViewModel: ViewModel
+    class MainWindowViewModel : ViewModel
     {
         #region Title
-        private string _Title = "My title";
+        private string _Title = "Learning English language";
 
         /// <summary> Title window</summary>
         public string Title
         {
             get => _Title;
-    
+
             set => Set(ref _Title, value);
 
         }
         #endregion
 
 
-        #region Commands
-
-
-        #region Close Application Command
-        public ICommand CloseApplicationCommand { get; }
-
-        private bool CanCloseApplicationCommandExecute(object p) => true;
-        private void OnCloseApplicationCommandExecuted(object p)
-        {
-            Application.Current.Shutdown();
-        }
-        #endregion
-
-
-
-        #endregion
 
         public MainWindowViewModel()
         {
-            #region Commands
-
-            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-
-
-            #endregion
         }
     }
 }
