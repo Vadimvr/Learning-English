6ff6f4a23d570a2574e038082b1929e6f7ed1e5e
MSI.windows <r.vadim.90@gmail.com>
2021-01-28 01:56:12 +0300

    Changing the data acquisition method

---
+++ TestConsole/CreatesValues.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class CreatesValues
+    {
+        public CreatesValues() { }
+        public static Values CredeData(string stringFromDB, string path)
+        {
+            string id = "";
+            string rus = "";
+            string eng = "";
+            int temp = 0;
+            for (int i = 0; i < stringFromDB.Length; i++)
+            {
+                if(stringFromDB[i] == '\t' && id == "")
+                {
+                    id = stringFromDB.Substring(temp, i);
+                    temp = i+1;
+                }
+                else if(stringFromDB[i] == '\t' && rus == "")
+                {
+                    rus = stringFromDB.Substring(temp, i-temp);
+                    temp = i+1;
+                }
+                else if (stringFromDB[i] == '\t' && eng == "")
+                {
+                    eng = stringFromDB.Substring(temp, i - temp);
+                    temp = i + 1;
+                    break;
+                }
+            }
+         
+            return new Values(rus,eng, stringFromDB[temp..], new PathToMultimedia(id, path));
+        }
+    }
+}
---
+++ TestConsole/PathToMultimedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class PathToMultimedia
+    {
+        public string PathImg { get; set; }
+        public string PathSong { get; set; }
+        public PathToMultimedia(string id, string path)
+        {
+            PathImg = path + @"\img\" + id + ".jpg";
+            PathSong = path + @"\song\" + id + ".mp3";
+        }
+    }
+}
