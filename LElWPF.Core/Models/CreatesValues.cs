ade286e375ba90bbc5499a5cb5fa7bbd63879ffc
MSI.windows <r.vadim.90@gmail.com>
2021-01-29 14:29:22 +0300

    changing the processing of the database

--- LElWPF.Core/Models/CreatesValues.cs
+++ LElWPF.Core/Models/CreatesValues.cs
@@ -12,28 +12,48 @@ namespace LElWPF.Core.Models
             string id = "";
             string rus = "";
             string eng = "";
+            string engT = "";
             int temp = 0;
             for (int i = 0; i < stringFromDB.Length; i++)
             {
-                if (stringFromDB[i] == '\t' && id == "")
+                if (stringFromDB[i] == '\t' && eng == "")
                 {
-                    id = stringFromDB.Substring(temp, i);
+                    eng = stringFromDB.Substring(temp, i);
                     temp = i + 1;
                 }
-                else if (stringFromDB[i] == '\t' && rus == "")
+                else if (stringFromDB[i] == '\t' && engT == "")
                 {
-                    rus = stringFromDB.Substring(temp, i - temp);
-                    temp = i + 1;
+                    engT = stringFromDB.Substring(temp, i - temp);
+                    temp = i + 1; break;
                 }
-                else if (stringFromDB[i] == '\t' && eng == "")
-                {
-                    eng = stringFromDB.Substring(temp, i - temp);
-                    temp = i + 1;
-                    break;
-                }
+               
             }
 
-            return new Values(rus, eng, stringFromDB[temp..], new PathToMultimedia(id, path));
+            //string id = "";
+            //string rus = "";
+            //string eng = "";
+            //int temp = 0;
+            //for (int i = 0; i < stringFromDB.Length; i++)
+            //{
+            //    if (stringFromDB[i] == '\t' && id == "")
+            //    {
+            //        id = stringFromDB.Substring(temp, i);
+            //        temp = i + 1;
+            //    }
+            //    else if (stringFromDB[i] == '\t' && rus == "")
+            //    {
+            //        rus = stringFromDB.Substring(temp, i - temp);
+            //        temp = i + 1;
+            //    }
+            //    else if (stringFromDB[i] == '\t' && eng == "")
+            //    {
+            //        eng = stringFromDB.Substring(temp, i - temp);
+            //        temp = i + 1;
+            //        break;
+            //    }
+            //}
+
+            return new Values(stringFromDB[temp..].Trim(), eng, engT, new PathToMultimedia(eng, path));
         }
     }
 }
