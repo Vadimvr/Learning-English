49f7cea764df41569f35c498c6a7d4de4e890e2a
MSI.windows <r.vadim.90@gmail.com>
2021-02-01 01:24:56 +0300

    added file name and path

--- LElWPF.Core/Models/OPenfileDialog/DefaultDialogService.cs
+++ LElWPF.Core/Models/OPenfileDialog/DefaultDialogService.cs
@@ -8,14 +8,19 @@ namespace LElWPF.Core.ViewModels.Base
 {
     public class DefaultDialogService : IDialogService
     {
+        public string File{ get; set; }
+        public string Path { get; set; }
         public string FilePath { get; set; }
-
+        
         public bool OpenFileDialog()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
                 FilePath = openFileDialog.FileName;
+                int temp = GetIndexFileName(openFileDialog.FileName);
+                Path = openFileDialog.FileName.Substring(0,temp);
+                File = openFileDialog.FileName[temp..];
                 return true;
             }
             return false;
@@ -30,6 +35,16 @@ namespace LElWPF.Core.ViewModels.Base
                 return true;
             }
             return false;
+        }
+        int GetIndexFileName(string filepath)
+        {
+            int temp = 0;
+            for (int i = 0; i < filepath.Length; i++)
+            {
+                if (filepath[i] == '\\')
+                    temp = i;
+            }
+            return temp+1;
         }
     }
 }
--- LElWPF.Core/Models/OPenfileDialog/IDialogService.cs
+++ LElWPF.Core/Models/OPenfileDialog/IDialogService.cs
@@ -6,8 +6,10 @@ namespace LElWPF.Core.ViewModels.Windows
 {
     public interface IDialogService
     {
-       
-        string FilePath { get; set; }  
+
+        string FilePath { get; set; }
+        string Path { get; set; }
+        string File { get; set; }
         bool OpenFileDialog(); 
         bool SaveFileDialog();  
     }
