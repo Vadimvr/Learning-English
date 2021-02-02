92d7004d95e0badd2dff5078e587d3567e6da816
MSI.windows <r.vadim.90@gmail.com>
2021-02-02 22:27:44 +0300

    Revert "try librory Microsoft.EntityFrameworkCore"
    This reverts commit 22b95f91455db5a28a6c848f949397ff6c5d2c71.

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,56 +1,80 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
+using Microsoft.Data.Sqlite;
 
 namespace TestConsole
 {
     class Program
     {
-        private static void Main()
+        static void Main(string[] args)
         {
-            using (var db = new BloggingContext())
+            Console.OutputEncoding = Encoding.UTF8;
+            ListValues values = new ListValues(@"D:\test\", "data.db");
+
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = @"D:\test\test.db";
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
             {
-                // Note: This sample requires the database to be created before running.
 
-                // Create
-                Console.WriteLine("Inserting a new blog");
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.SaveChanges();
+                connection.Open();
+                var delTableCmd = connection.CreateCommand();
+                delTableCmd.CommandText = "DROP TABLE IF EXISTS 'Values1'";
+                delTableCmd.ExecuteNonQuery();
 
-                // Read
-                Console.WriteLine("Querying for a blog");
-            
-                var blog =from i in db.Blogs 
-                          where i.BlogId == 5  
-                          select i;
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = "CREATE TABLE 'Values1'(" +
+                        "'id'	INTEGER NOT NULL UNIQUE ," +
+                        "'eng'  VARCHAR(50) NOT NULL UNIQUE," +
+                        "'engt'  VARCHAR(50)," +
+                        "'rus'  VARCHAR(100) NOT NULL," +
+                        "'paths'  VARCHAR(50)," +
+                        "'pathi'   VARCHAR(50)," +
+                        "PRIMARY KEY('id' AUTOINCREMENT))";
+                createTableCmd.ExecuteNonQuery();
 
 
-                foreach (var item in db.Blogs)
+                using (var transaction = connection.BeginTransaction())
                 {
-                    Console.WriteLine(item.BlogId);
-                }
-                // Update
-                Console.WriteLine("Updating the blog and adding a post");
-                //blog.Url = "https://devblogs.microsoft.com/dotnet";
-                //blog.Posts.Add(
-                //    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-                //db.SaveChanges();
 
-                // Delete
-                Console.WriteLine("Delete the blog");
-                db.Remove(blog);
-                db.SaveChanges();
+                    //values.Data.Count Values v = values.Data[i];
+                    for (int i = 0; i < values.Data.Count; i++)
+                    {
+                        var insertCmd = connection.CreateCommand();
+                        insertCmd.CommandText = "INSERT INTO [Values1] ([eng], [engt], [rus],[paths],[pathi])"
+                            + $"VALUES('{values.Data[i].Eng}', '{values.Data[i].EngTranscription}', '{values.Data[i].Rus}', '{values.Data[i].Song}', '{values.Data[i].Img}')";
+                        try
+                        {
+                            insertCmd.ExecuteNonQuery();
+                        }
+                        catch (Microsoft.Data.Sqlite.SqliteException)
+                        {
+
+                            Console.WriteLine($"не удалось добавить {values.Data[i].Eng}");
+                        }
+
+                    }
+                    transaction.Commit();
+                }
+                var selectCmd = connection.CreateCommand();
+                selectCmd.CommandText = "SELECT COUNT(*) FROM Values1";
+                Console.WriteLine(selectCmd.ExecuteScalar());
+                selectCmd.CommandText = "SELECT eng,rus FROM Values1 WHERE id= 20";
+                Console.WriteLine(selectCmd.ExecuteScalar());
+                
+
+
+                selectCmd.CommandText = "SELECT COUNT(*) FROM Values1";
+                Console.WriteLine(selectCmd.ExecuteScalar());
+                connection.Close();
+                Console.WriteLine("end");
+                Console.ReadKey();
+
             }
         }
     }
 }
-
 //CREATE TABLE "Values" (
 //	"id"	INTEGER NOT NULL UNIQUE,
 //	"EnglishWord"	TEXT NOT NULL UNIQUE,
@@ -72,65 +96,3 @@ namespace TestConsole
 //        "'PathImg'   TEXT," +
 //        "PRIMARY KEY('id' AUTOINCREMENT))";
 //createTableCmd.ExecuteNonQuery();
-
-
-//Console.OutputEncoding = Encoding.UTF8;
-//ListValues values = new ListValues(@"D:\test\", "data.db");
-
-
-//var connectionStringBuilder = new SqliteConnectionStringBuilder();
-//connectionStringBuilder.DataSource = @"D:\test\test.db";
-
-//using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
-//{
-
-//    connection.Open();
-//    var delTableCmd = connection.CreateCommand();
-//    delTableCmd.CommandText = "DROP TABLE IF EXISTS 'Values1'";
-//    delTableCmd.ExecuteNonQuery();
-
-//    var createTableCmd = connection.CreateCommand();
-//    createTableCmd.CommandText = "CREATE TABLE 'Values1'(" +
-//            "'id'	INTEGER NOT NULL UNIQUE ," +
-//            "'eng'  VARCHAR(50) NOT NULL UNIQUE," +
-//            "'engt'  VARCHAR(50)," +
-//            "'rus'  VARCHAR(100) NOT NULL," +
-//            "'paths'  VARCHAR(50)," +
-//            "'pathi'   VARCHAR(50)," +
-//            "PRIMARY KEY('id' AUTOINCREMENT))";
-//    createTableCmd.ExecuteNonQuery();
-
-
-//    using (var transaction = connection.BeginTransaction())
-//    {
-
-//        //values.Data.Count Values v = values.Data[i];
-//        for (int i = 0; i < values.Data.Count; i++)
-//        {
-//            var insertCmd = connection.CreateCommand();
-//            insertCmd.CommandText = "INSERT INTO [Values1] ([eng], [engt], [rus],[paths],[pathi])"
-//                + $"VALUES('{values.Data[i].Eng}', '{values.Data[i].EngTranscription}', '{values.Data[i].Rus}', '{values.Data[i].Song}', '{values.Data[i].Img}')";
-//            try
-//            {
-//                insertCmd.ExecuteNonQuery();
-//            }
-//            catch (Microsoft.Data.Sqlite.SqliteException)
-//            {
-
-//                Console.WriteLine($"не удалось добавить {values.Data[i].Eng}");
-//            }
-
-//        }
-//        transaction.Commit();
-//    }
-//    var selectCmd = connection.CreateCommand();
-//    selectCmd.CommandText = "SELECT COUNT(*) FROM Values1";
-//    Console.WriteLine(selectCmd.ExecuteScalar());
-//    selectCmd.CommandText = "SELECT eng,rus FROM Values1 WHERE id= 20";
-//    Console.WriteLine(selectCmd.ExecuteScalar());
-
-
-
-//    selectCmd.CommandText = "SELECT COUNT(*) FROM Values1";
-//    Console.WriteLine(selectCmd.ExecuteScalar());
-//    connection.Close();
