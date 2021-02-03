9f516ea6d4ae79449335139bf1d2b5a4028dc493
MSI.windows <r.vadim.90@gmail.com>
2021-02-04 01:22:40 +0300

    Added  DatabaseWindow and  DatabaseWindowViewModel

---
+++ LElWPF.Core/ViewModels/DatabaseWindow/DatabaseWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LElWPF.Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LElWPF.Core.ViewModels.DatabaseWindow
+{
+    class DatabaseWindowViewModel : ViewModel
+    {
+        public DatabaseWindowViewModel()
+        {
+           
+        }
+    }
+}
