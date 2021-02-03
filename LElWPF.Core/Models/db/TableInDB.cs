d1a90f322627c783d10e22bc8e32e616b491d162
MSI.windows <r.vadim.90@gmail.com>
2021-02-04 02:47:47 +0300

    add class TableInDB

---
+++ LElWPF.Core/Models/db/TableInDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LElWPF.Core.Models.db
+{
+    class TableInDB
+    {
+        public string NameTable { get; set; }
+        public List<Values> Values { get; set; }
+        public TableInDB(string nameTable, List<Values> values)
+        {
+            NameTable = nameTable;
+            Values = values;
+        }
+    }
+}
