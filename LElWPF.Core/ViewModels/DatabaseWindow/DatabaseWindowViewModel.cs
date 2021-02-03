0976e320ce7165a37661afad651627fe14e62bed
MSI.windows <r.vadim.90@gmail.com>
2021-02-04 02:49:27 +0300

    displaying the entire database

--- LElWPF.Core/ViewModels/DatabaseWindow/DatabaseWindowViewModel.cs
+++ LElWPF.Core/ViewModels/DatabaseWindow/DatabaseWindowViewModel.cs
@@ -1,15 +1,34 @@
 ﻿using LElWPF.Core.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using LElWPF.Core.Models.db;
 
 namespace LElWPF.Core.ViewModels.DatabaseWindow
 {
     class DatabaseWindowViewModel : ViewModel
     {
+        public ObservableCollection<TableInDB> AllDB { get; }
+
+        #region SelectedTable
+
+        
+        private TableInDB _SelectedTable;
+
+       
+        public TableInDB SelectedTable
+        {
+            get => _SelectedTable;
+            set => Set(ref _SelectedTable, value);
+        }
+
+        #endregion
         public DatabaseWindowViewModel()
         {
-           
+
+            СreationTableInDB сreationTableInDB = new СreationTableInDB(@"D:\test\", "test2.db");
+            AllDB = сreationTableInDB.GetFullBase();
         }
     }
 }
