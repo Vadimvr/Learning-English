92bab1783f775282277ff18f612ec78af9deffea
Vadim Rakovets <r.vadim.90@gmail.com>
2021-04-30 21:48:05 +0300

    adding words from a text file

---
+++ LElWPF.Core/Models/AddWordsFromFile/WordS.cs
@@ -0,0 +1,15 @@
+﻿namespace LElWPF.Core.Models.AddWordsFromFile
+{
+    class WordS
+    {
+        public string Eng { get; set; }
+        public string Rus { get; set; }
+        public string EngTranscription { get; set; }
+        public WordS(string eng, string rus)
+        {
+            Eng = eng;
+            Rus = rus;
+            EngTranscription = "";
+        }
+    }
+}
